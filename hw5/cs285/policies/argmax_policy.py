defc486df21b15779b8137f4a22c44f153fe0d6f
Lucy Shi <lucys@usc.edu>
2021-02-18 17:33:10 +0800

    hw5

---
+++ hw5/cs285/policies/argmax_policy.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pdb
+
+
+class ArgMaxPolicy(object):
+
+    def __init__(self, critic):
+        self.critic = critic
+
+    def set_critic(self, critic):
+        self.critic = critic
+
+    def get_action(self, obs):
+        # MJ: changed the dimension check to a 3
+        if len(obs.shape) > 3:
+            observation = obs
+        else:
+            observation = obs[None]
+
+        # TODO: get this from hw3
+        q_values = self.critic.qa_values(observation)
+        action = q_values.max(1)
+
+        return action.squeeze()
+
+    ####################################
+    ####################################
